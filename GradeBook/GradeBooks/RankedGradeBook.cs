c73e3d8b16d0f4984f360f3571899d25ff4d9a46
Chris Liechty <christopher.j.liechty@aruplab.com>
2020-01-02 09:56:28 -0700

    Complete Module 3

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -1,10 +1,39 @@
-﻿namespace GradeBook.GradeBooks
+﻿using System;
+
+namespace GradeBook.GradeBooks
 {
     public class RankedGradeBook : BaseGradeBook
     {
         public RankedGradeBook(string name) : base(name)
         {
             Type = Enums.GradeBookType.Ranked;
+        }
+
+        public override char GetLetterGrade(double averageGrade)
+        {
+            if (this.Students.Count < 5)
+            {
+                throw new InvalidOperationException("Ranked-grading requires a minimum of 5 students to work");
+            }
+            var percentage = averageGrade * .2;
+            if (percentage >= 20)
+            {
+                return 'A';
+            }
+            if (percentage >= 15)
+            {
+                return 'B';
+            }
+            if (percentage >= 10)
+            {
+                return 'C';
+            }
+            if (percentage >= 5)
+            {
+                return 'D';
+            }
+
+            return 'F';
         }
     }
 }
